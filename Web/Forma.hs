49e683dc19f6adb206b1e15e702d3c706caa6b0b
mrkkrp <markkarpov92@gmail.com>
2017-05-22 20:02:42 +0700

    Update my email address
    [skip ci]

--- Web/Forma.hs
+++ Web/Forma.hs
@@ -3,7 +3,7 @@
 -- Copyright   :  © 2017 Mark Karpov
 -- License     :  BSD 3 clause
 --
--- Maintainer  :  Mark Karpov <markkarpov@openmailbox.org>
+-- Maintainer  :  Mark Karpov <markkarpov92@gmail.com>
 -- Stability   :  experimental
 -- Portability :  portable
 --
